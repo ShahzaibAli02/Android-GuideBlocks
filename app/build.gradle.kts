943bce8029a96e34ced3e861fbc96d5b46b90cfd
shahzaibali02 <alishahzaib02@gmail.com>
2024-02-22 14:44:12 +0500

    -> Added Next Previous , Dismiss , Complete Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("maven-publish")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -55,7 +56,7 @@ java {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17    
+    sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
 
@@ -76,11 +77,12 @@ dependencies {
     implementation("nl.dionsegijn:konfetti-xml:2.0.4")
     val appcompat_version = "1.6.1"
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     implementation("androidx.appcompat:appcompat:$appcompat_version")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
 
     //NEW CHANGES
     //COMPOSE CODE
@@ -88,6 +90,7 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -98,9 +101,13 @@ dependencies {
         exclude(group = "com.github.bumptech.glide")
         exclude(group = "androidx.room")
     }
-
     implementation("com.google.code.gson:gson:2.8.8")
+
+    //coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-gif:2.4.0")
+
+    //scanner
+    implementation("com.google.mlkit:barcode-scanning:17.0.3")
     implementation("com.trafi:rating-seek-bar:0.4-alpha")
-
-
 }
